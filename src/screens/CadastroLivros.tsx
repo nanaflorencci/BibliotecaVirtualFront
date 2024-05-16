475ce7c6e8e136283862028a15b2b49960cb5c3f
nanaflorencci <giovannaflorencci1814@gmail.com>
2024-05-16 15:40:18 -0300

    .

--- src/screens/CadastroLivros.tsx
+++ src/screens/CadastroLivros.tsx
@@ -1,56 +1,121 @@
-import React from "react";
-import { FlatList, Image, StatusBar, StyleSheet, Text, TextInput, TextInputBase, TouchableOpacity, View } from "react-native";
+import axios from "axios";
+import React, { useState } from "react";
+import {ScrollView, Image, StatusBar, StyleSheet, Text, TextInput,TouchableOpacity, View } from "react-native";
 
-interface Livros {
-    titulo: string;
-    autor: string;
-    data_lancamento: string;
-    editora: string;
-    sinopse: string,
-    avaliacao: string,
-    image: any;
+const CadastroLivros: React.FC = () => {
+    const [titulo, setTitulo] = useState<string>('');
+    const [autor, setAutor] = useState<string>('');
+    const [genero, setGenero] = useState<string>('');
+    const [data_lancamento, setDataLancamento] = useState<string>('');
+    const [editora, setEditora] = useState<string>('');
+    const [sinopse, setSinopse] = useState<string>('');
+    const [avaliacao, setAvaliacao] = useState<string>('');
+    const [imagem, setImagem] = useState<any>('');
+
+    const CadastroProduto = async () => {
+        try{
+        const formData=new FormData();
+        formData.append('titulo', titulo);
+        formData.append('autor', autor);
+        formData.append('genero', genero);
+        formData.append('data_lancamento', data_lancamento);
+        formData.append('editora', editora);
+        formData.append('sinopse', sinopse);
+        formData.append('avaliacao', avaliacao);
+        formData.append('imagem',{
+            uri:imagem,
+            type:'image/jpeg',
+            name:new Date()+ '.jpg',
+        });
+
+const response= await axios.post('http://10.137.11.217:8000/api/clientes', formData,{
+    headers:{
+        'Content-Type':'multipart/form-data'
+    }
+});
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <ScrollView>
+            <StatusBar backgroundColor="#000000" barStyle="light-content" />
+
+            <View style={styles.form}>
+
+            <TextInput
+                    style={styles.input}
+                    placeholder="Título do livro:"
+                    value={titulo}
+                    onChangeText={setTitulo}/>
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Autor do livro:"
+                    value={autor}
+                    onChangeText={setAutor}/>
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Gênero do livro:"
+                    value={genero}
+                    onChangeText={setGenero}
+                    multiline />
+                    
+                <TextInput
+                    style={styles.input}
+                    placeholder="Data de lançamento do livro:"
+                    value={data_lancamento}
+                    onChangeText={setDataLancamento}
+                    multiline />
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Editora do livro:"
+                    value={editora}
+                    onChangeText={setEditora}
+                    multiline />
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Sinopse do livro:"
+                    value={sinopse}
+                    onChangeText={setSinopse}
+                    multiline />
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Avaliação do livro:"
+                    value={avaliacao}
+                    onChangeText={setAvaliacao}
+                    multiline />
+
+            </View>
+            </ScrollView>
+        </View>
+    );
 }
-
-const dados: Livros[] = [
-    {titulo: 'Coraline',
-    autor: 'Neil Gainman',
-    data_lancamento: '2002-07-02',
-    editora:  'Intrínseca',
-    sinopse: 'É um conto no qual uma jovem descobre uma porta para um misterioso mundo no palácio para onde acabou de se mudar.',
-    avaliacao: '',
-    image: require('')},
-];
-
-const renderItem = ({ item }: { item: Livros }) => (
-    <TouchableOpacity style={styles.item}>
-        <Text style={styles.itemTitle}>{item.titulo}</Text>
-        <Text style={styles.textInformacoes}>{item.autor}, {item.avaliacao},{item.data_lancamento},{item.editora},{item.sinopse}</Text>
-        <Image source={item.image} style={styles.image} />
-        <Text style={styles.textAvaliacao}>{item.avaliacao}</Text>
-    </TouchableOpacity>
-
-);
-
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#000000' ,
         flex: 1
     },
-    item: {
-
+    input: {
+        height: 40,
+        borderColor: '#000000',
+        borderWidth: 3,
+        marginBottom: 5,
+        paddingHorizontal: 10,
+        borderRadius: 10
     },
-    itemTitle: {
-
+    form: {
+        marginTop: 10,
+        padding: 15,
+        backgroundColor: '#FFF',
+        borderRadius: 10,
     },
-    textInformacoes: {
-
-    },
-    image: {
-
-    },
-    textAvaliacao: {
-        
-    }
 });
 
-export default Livros;
+export default CadastroLivros;
